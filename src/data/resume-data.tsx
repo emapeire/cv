4fddc3606d3e9179ad40fe803ccd5a26b49137b1
Emanuel Peire <emapeire@outlook.com>
2024-01-03 21:52:36 -0300

    Update project files and remove unused assets

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,156 @@
-import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-} from "@/images/logos";
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import { GitHubIcon, LinkedInIcon, XIcon } from '@/components/icons'
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
-  summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
-  contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
-    social: [
-      {
-        name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
-        icon: GitHubIcon,
-      },
-      {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
-        icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
-      },
-    ],
-  },
-  education: [
-    {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
-    },
-  ],
-  work: [
-    {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
-    },
-  ],
-  skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
-  ],
-  projects: [
-    {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
-  ],
-} as const;
+	name: 'Emanuel Peire',
+	initials: 'EP',
+	location: 'Rosario, Argentina',
+	locationLink: 'https://www.google.com/maps/place/Rosario',
+	about:
+		'Frontend Engineer, OSS Developer, LLMs technical researcher and DX Architect. Always influenced by design.',
+	summary: `As a Front-end Engineer, I specialize in web development and have a background in Machine Learning research.
+	My passion lies in crafting innovative solutions that blend technology and design with a focus on social impact, aiming to make a meaningful difference in people's lives.
+	I am dedicated to utilizing the most recent and advanced frontend technologies to improve user accessibility and maximize web application performance.
+	My goal is to provide a smooth and effective user experience.`,
+	avatarUrl: 'https://unavatar.io/github/emapeire',
+	personalWebsiteUrl: 'https://linktr.ee/emapeire',
+	contact: {
+		email: 'emapeire@outlook.com',
+		tel: '+5493417190449',
+		social: [
+			{
+				name: 'GitHub',
+				url: 'https://github.com/emapeire',
+				icon: GitHubIcon
+			},
+			{
+				name: 'LinkedIn',
+				url: 'https://www.linkedin.com/in/emapeire',
+				icon: LinkedInIcon
+			},
+			{
+				name: 'X',
+				url: 'https://x.com/emapeire',
+				icon: XIcon
+			}
+		]
+	},
+	education: [
+		{
+			school: 'Universidad Nacional de Rosario',
+			degree: "Bachelor's Degree in Industrial Design",
+			start: '2019',
+			end: '2021'
+		}
+	],
+	work: [
+		{
+			company: 'Smat SA.',
+			link: 'https://smat.io',
+			badges: ['Remote'],
+			title: 'QA Tester and Frontend Developer',
+			logo: 'https://assets-global.website-files.com/63055fe72615549c6cfae8e0/63c55f389bcbb423249e730f_Frame%20560.png',
+			start: '2022',
+			end: '2023',
+			description: `Spearheaded comprehensive QA testing processes, employing tools like Jest, and Playwright to ensure seamless functionality and performance of web applications.
+			Collaborated with cross-functional teams to implement Test-Driven Development (TDD) methodologies, optimizing code quality and reducing bugs.
+			Contributed to the frontend development of user-centric interfaces using React.js, enhancing user experiences through intuitive designs and responsive layouts.`
+		}
+	],
+	skills: [
+		'Android Studio',
+		'Astro',
+		'CSS',
+		'Express',
+		'Figma',
+		'Git (Bash)',
+		'GNU/Linux',
+		'HTML',
+		'JavaScript',
+		'Jest',
+		'ZSH',
+		'MongoDB',
+		'Next.js',
+		'Node.js',
+		'Notion',
+		'Playwright',
+		'Postman',
+		'Prisma',
+		'Python',
+		'React Native',
+		'React.js',
+		'RESTful APIs',
+		'SQL (MySQL, PostgreSQL)',
+		'Supabase',
+		'TypeScript',
+		'Vercel',
+		'Vite',
+		'Vitest',
+		'WordPress',
+		'Xcode'
+	],
+	projects: [
+		{
+			title: 'SWC Router',
+			techStack: [
+				'Side Project',
+				'Open Source',
+				'React',
+				'JavaScript',
+				'HTML',
+				'Vite',
+				'CSS',
+				'SWC'
+			],
+			description:
+				'Simple single-page applications (SPA) with ease using swc-router. This lightweight routing solution for React applications leverages the speed of SWC.',
+			logo: '',
+			link: {
+				label: 'npmjs.com/package/swc-router',
+				href: 'https://npmjs.com/package/swc-router'
+			}
+		},
+		{
+			title: 'Eloquent JavaScript Chat',
+			techStack: [
+				'Side Project',
+				'Open Source',
+				'React',
+				'TypeScript',
+				'Next.js',
+				'Tailwind CSS',
+				'Shadcn/ui',
+				'Llama2',
+				'OpenAI',
+				'GPT'
+			],
+			description:
+				'In this app you will be able to interact with a intelligent llama that will answer your questions about JavaScript.',
+			logo: '',
+			link: {
+				label: 'github.com/emapeire/eloquent-js-chat',
+				href: 'https://github.com/emapeire/eloquent-js-chat'
+			}
+		},
+		{
+			title: 'Image 2 Code',
+			techStack: [
+				'Side Project',
+				'Open Source',
+				'React',
+				'TypeScript',
+				'Next.js',
+				'Tailwind CSS',
+				'Shadcn/ui',
+				'OpenAI',
+				'GPT'
+			],
+			description:
+				'This is a tool that converts images to code. It uses AI to generate code from images. It is based on the GPT-4-vision-preview model.',
+			logo: '',
+			link: {
+				label: 'github.com/emapeire/image-to-code',
+				href: 'https://github.com/emapeire/image-to-code'
+			}
+		}
+	]
+} as const
