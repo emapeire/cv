a7093ac39ca6fc0d99fe21c333785cd5d9227590
Emanuel Peire <emapeire@outlook.com>
2024-01-03 23:07:08 -0300

    Add Head component and update layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import React from 'react'
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 import './globals.css'
-import React from 'react'
 
 export const metadata: Metadata = {
 	title: 'CV - Emanuel Peire',
@@ -19,8 +20,14 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en' className={`${inter.className}`}>
-			<body>{children}</body>
-			<Analytics />
+			<Head>
+				<meta charSet='utf-8' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<body>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	)
 }
