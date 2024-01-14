3617d4f05160fdec7335ba55535299db99b6dd5f
Emanuel Peire <emapeire@outlook.com>
2024-01-14 15:17:15 -0300

    Update meta tag in HeadIcons component

--- src/components/head-icons.tsx
+++ src/components/head-icons.tsx
@@ -6,7 +6,7 @@ export function HeadIcons({ metadata }: { metadata: Metadata }) {
 		<Head>
 			<meta charSet='utf-8' />
 			<meta name='viewport' content='width=device-width, initial-scale=1' />
-			<meta name='title' title={metadata.title?.toString()} />
+			<meta title={metadata.title?.toString()} />
 			<meta name='description' content={metadata.description?.toString()} />
 			<meta name='author' content='Emanuel Peire' />
 			<meta name='robots' content='index, follow' />
