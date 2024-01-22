091a462e1d0dc2fb91550882013f4341caa72716
Emanuel Peire <emapeire@outlook.com>
2024-01-22 20:38:20 -0300

    Add async headers function to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Permissions-Policy',
+						value: 'interest-cohort=()'
+					}
+				]
+			}
+		]
+	}
+}
 
 module.exports = nextConfig
