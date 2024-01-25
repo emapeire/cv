b740b47c384f26ce6d4020b75c59061655a37eaa
Emanuel Peire <emapeire@outlook.com>
2024-01-25 12:59:48 -0300

    Refactor contact information and data access in Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,42 +9,44 @@ import { GlobeIcon, MailIcon, PhoneIcon } from 'lucide-react'
 import { RESUME_DATA } from '@/data/resume-data'
 
 export default function Page() {
+	const data = RESUME_DATA
+
 	return (
 		<main className='container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16'>
 			<section className='mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6'>
 				<div className='flex items-center justify-between'>
 					<div className='flex-1 space-y-1.5'>
-						<h1 className='text-2xl font-bold'>{RESUME_DATA.name}</h1>
+						<h1 className='text-2xl font-bold'>{data.name}</h1>
 						<p className='max-w-md text-pretty font-mono text-sm text-muted-foreground'>
-							{RESUME_DATA.about}
+							{data.about}
 						</p>
 						<p className='max-w-md items-center text-pretty font-mono text-xs text-muted-foreground'>
 							<a
 								className='inline-flex gap-x-1.5 align-baseline leading-none hover:underline'
-								href={RESUME_DATA.locationLink}
+								href={data.locationLink}
 								target='_blank'
 								rel='noreferrer'
 							>
 								<GlobeIcon className='h-3 w-3' />
-								{RESUME_DATA.location}
+								{data.location}
 							</a>
 						</p>
 						<div className='flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden'>
-							{RESUME_DATA.contact.email ? (
+							{data.contact.email ? (
 								<Button className='h-8 w-8' variant='outline' size='icon' asChild>
-									<a href={`mailto:${RESUME_DATA.contact.email}`} target='_blank' rel='noreferrer'>
+									<a href={`mailto:${data.contact.email}`} target='_blank' rel='noreferrer'>
 										<MailIcon className='h-4 w-4' />
 									</a>
 								</Button>
 							) : null}
-							{RESUME_DATA.contact.tel ? (
+							{data.contact.tel ? (
 								<Button className='h-8 w-8' variant='outline' size='icon' asChild>
-									<a href={`tel:${RESUME_DATA.contact.tel}`} target='_blank' rel='noreferrer'>
+									<a href={`tel:${data.contact.tel}`} target='_blank' rel='noreferrer'>
 										<PhoneIcon className='h-4 w-4' />
 									</a>
 								</Button>
 							) : null}
-							{RESUME_DATA.contact.social.map((social) => (
+							{data.contact.social.map((social) => (
 								<Button key={social.name} className='h-8 w-8' variant='outline' size='icon' asChild>
 									<a href={social.url} target='_blank' rel='noreferrer'>
 										<social.icon className='h-4 w-4' />
@@ -53,33 +55,31 @@ export default function Page() {
 							))}
 						</div>
 						<div className='hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex'>
-							{RESUME_DATA.contact.email ? (
-								<a href={`mailto:${RESUME_DATA.contact.email}`} target='_blank' rel='noreferrer'>
-									<span className='underline'>{RESUME_DATA.contact.email}</span>
+							{data.contact.email ? (
+								<a href={`mailto:${data.contact.email}`} target='_blank' rel='noreferrer'>
+									<span className='underline'>{data.contact.email}</span>
 								</a>
 							) : null}
-							{RESUME_DATA.contact.tel ? (
-								<a href={`tel:${RESUME_DATA.contact.tel}`} target='_blank' rel='noreferrer'>
-									<span className='underline'>{RESUME_DATA.contact.tel}</span>
+							{data.contact.tel ? (
+								<a href={`tel:${data.contact.tel}`} target='_blank' rel='noreferrer'>
+									<span className='underline'>{data.contact.tel}</span>
 								</a>
 							) : null}
 						</div>
 					</div>
 
 					<Avatar className='h-28 w-28'>
-						<AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatar} />
-						<AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
+						<AvatarImage alt={data.name} src={data.avatar} />
+						<AvatarFallback>{data.initials}</AvatarFallback>
 					</Avatar>
 				</div>
 				<Section>
 					<h2 className='text-xl font-bold'>About</h2>
-					<p className='text-pretty font-mono text-sm text-muted-foreground'>
-						{RESUME_DATA.summary}
-					</p>
+					<p className='text-pretty font-mono text-sm text-muted-foreground'>{data.summary}</p>
 				</Section>
 				<Section>
 					<h2 className='text-xl font-bold'>Work Experience</h2>
-					{RESUME_DATA.work.map((work) => {
+					{data.work.map((work) => {
 						return (
 							<Card key={work.company}>
 								<CardHeader>
@@ -116,7 +116,7 @@ export default function Page() {
 				</Section>
 				<Section>
 					<h2 className='text-xl font-bold'>Education</h2>
-					{RESUME_DATA.education.map((education) => {
+					{data.education.map((education) => {
 						return (
 							<Card key={education.school}>
 								<CardHeader>
@@ -135,7 +135,7 @@ export default function Page() {
 				<Section>
 					<h2 className='text-xl font-bold'>Skills</h2>
 					<div className='flex flex-wrap gap-1'>
-						{RESUME_DATA.skills.map((skill) => {
+						{data.skills.map((skill) => {
 							return <Badge key={skill}>{skill}</Badge>
 						})}
 					</div>
@@ -144,7 +144,7 @@ export default function Page() {
 				<Section className='print-force-new-page scroll-mb-16'>
 					<h2 className='text-xl font-bold'>Projects</h2>
 					<div className='-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3'>
-						{RESUME_DATA.projects.map((project) => {
+						{data.projects.map((project) => {
 							return (
 								<ProjectCard
 									key={project.title}
@@ -162,10 +162,10 @@ export default function Page() {
 			<CommandMenu
 				links={[
 					{
-						url: RESUME_DATA.personalWebsiteUrl,
+						url: data.personalWebsiteUrl,
 						title: 'Personal Website'
 					},
-					...RESUME_DATA.contact.social.map((socialMediaLink) => ({
+					...data.contact.social.map((socialMediaLink) => ({
 						url: socialMediaLink.url,
 						title: socialMediaLink.name
 					}))
