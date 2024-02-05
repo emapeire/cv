28818493eba92f057b858dc96e28fd3a530a3259
Emanuel Peire <emapeire@outlook.com>
2024-02-05 14:02:51 -0300

    Changing link .ico config

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -14,9 +14,10 @@ export function Head({ metadata }: { metadata: Metadata }) {
 			<meta name='msapplication-TileColor' content='#000000' />
 			<meta name='theme-color' content='#000000'></meta>
 
-			<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+			<link rel='icon' type='image/x-icon' href='/favicon.ico' />
 			<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
 			<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+			<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
 			<link rel='manifest' href='/site.webmanifest' />
 		</head>
 	)
