86b020a492f18ef017096a039bad38b1683e4f7e
Emanuel Peire <emapeire@outlook.com>
2024-04-18 13:50:50 -0300

    Refactor contact links and personal website URL in page.tsx and resume-data.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Card, CardHeader, CardContent } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
-import { CommandMenu } from '@/components/command-menu'
 import { Section } from '@/components/ui/section'
-import { Button } from '@/components/ui/button'
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { ButtonLink } from '@/components/button-link'
 import { ProjectCard } from '@/components/project-card'
-import { GlobeIcon, MailIcon, PhoneIcon, AppWindowIcon, LinkIcon } from 'lucide-react'
+import { CommandMenu } from '@/components/command-menu'
+import { GlobeIcon } from 'lucide-react'
 import { data } from '@/constants'
 
 export default function Page() {
@@ -31,106 +30,7 @@ export default function Page() {
 							</a>
 						</p>
 
-						<div className='flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden'>
-							{data.personalWebsiteUrl && (
-								<Tooltip>
-									<TooltipTrigger asChild>
-										<Button className='size-8' variant='outline' size='icon' asChild>
-											<a href={data.personalWebsiteUrl.url} target='_blank' rel='noreferrer'>
-												<AppWindowIcon className='size-4' />
-											</a>
-										</Button>
-									</TooltipTrigger>
-									<TooltipContent side='bottom'>
-										<p className='text-xs'>{data.personalWebsiteUrl.name}</p>
-									</TooltipContent>
-								</Tooltip>
-							)}
-							{data.contact.email && (
-								<Tooltip>
-									<TooltipTrigger asChild>
-										<Button className='size-8' variant='outline' size='icon' asChild>
-											<a href={`mailto:${data.contact.email.at}`} target='_blank' rel='noreferrer'>
-												<MailIcon className='size-4' />
-											</a>
-										</Button>
-									</TooltipTrigger>
-									<TooltipContent side='bottom'>
-										<p className='text-xs'>{data.contact.email.name}</p>
-									</TooltipContent>
-								</Tooltip>
-							)}
-							{data.contact.tel && (
-								<Tooltip>
-									<TooltipTrigger asChild>
-										<Button className='size-8' variant='outline' size='icon' asChild>
-											<a
-												href={`tel:${data.contact.tel.phoneNumber}`}
-												target='_blank'
-												rel='noreferrer'
-											>
-												<PhoneIcon className='size-4' />
-											</a>
-										</Button>
-									</TooltipTrigger>
-									<TooltipContent side='bottom'>
-										<p className='text-xs'>{data.contact.tel.name}</p>
-									</TooltipContent>
-								</Tooltip>
-							)}
-							{data.contact.social.map((social) => (
-								<Tooltip key={social.name}>
-									<TooltipTrigger asChild>
-										<Button
-											key={social.name}
-											className='size-8'
-											variant='outline'
-											size='icon'
-											asChild
-										>
-											<a href={social.url} target='_blank' rel='noreferrer'>
-												<social.icon className='size-4' />
-											</a>
-										</Button>
-									</TooltipTrigger>
-									<TooltipContent side='bottom'>
-										<p className='text-xs'>{social.name}</p>
-									</TooltipContent>
-								</Tooltip>
-							))}
-							{data.contact.link && (
-								<Tooltip>
-									<TooltipTrigger asChild>
-										<Button className='size-8' variant='outline' size='icon' asChild>
-											<a href={data.contact.link.url} target='_blank' rel='noreferrer'>
-												<LinkIcon className='size-4' />
-											</a>
-										</Button>
-									</TooltipTrigger>
-									<TooltipContent side='bottom'>
-										<p className='text-xs'>{data.contact.link.name}</p>
-									</TooltipContent>
-								</Tooltip>
-							)}
-						</div>
-
-						<div className='hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex'>
-							{data.personalWebsiteUrl && (
-								<a href={data.personalWebsiteUrl.url} target='_blank' rel='noreferrer'>
-									<span className='underline'>{data.personalWebsiteUrl.url}</span>
-								</a>
-							)}
-							{data.contact.email && (
-								<a href={`mailto:${data.contact.email.at}`} target='_blank' rel='noreferrer'>
-									<span className='underline'>{data.contact.email.at}</span>
-								</a>
-							)}
-							{data.contact.tel && (
-								<a href={`tel:${data.contact.tel.phoneNumber}`} target='_blank' rel='noreferrer'>
-									<span className='underline'>{data.contact.tel.phoneNumber}</span>
-								</a>
-							)}
-						</div>
+						<ButtonLink data={data} />
 					</div>
 
 					<a href='https://github.com/emapeire' target='_blank' rel='noreferrer'>
