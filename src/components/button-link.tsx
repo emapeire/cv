a0fe881ca0dd43c6227007c5e52b686159233b53
Emanuel Peire <emapeire@outlook.com>
2024-04-18 14:03:47 -0300

    Refactor contact links and personal website URL in ButtonLink component and related files

--- src/components/button-link.tsx
+++ src/components/button-link.tsx
@@ -5,15 +5,36 @@ import { type ResumeData } from '@/types'
 
 export function ButtonLink({ data }: { data: ResumeData }) {
 	const linkData = [
-		{ url: data.personalWebsiteUrl?.url, icon: AppWindowIcon, name: data.personalWebsiteUrl?.name },
-		{ url: `mailto:${data.contact.email?.at}`, icon: MailIcon, name: data.contact.email?.name },
-		{ url: `tel:${data.contact.tel?.phoneNumber}`, icon: PhoneIcon, name: data.contact.tel?.name },
+		{
+			url: data.personalWebsiteUrl?.url,
+			icon: AppWindowIcon,
+			name: data.personalWebsiteUrl?.name,
+			type: 'website'
+		},
+		{
+			url: `mailto:${data.contact.email?.at}`,
+			icon: MailIcon,
+			name: data.contact.email?.name,
+			type: 'email'
+		},
+		{
+			url: `tel:${data.contact.tel?.phoneNumber}`,
+			icon: PhoneIcon,
+			name: data.contact.tel?.name,
+			type: 'phone'
+		},
 		...data.contact.social.map((social) => ({
 			url: social.url,
 			icon: social.icon,
-			name: social.name
+			name: social.name,
+			type: 'social'
 		})),
-		{ url: data.contact.link?.url, icon: LinkIcon, name: data.contact.link?.name }
+		{
+			url: data.contact.link?.url,
+			icon: LinkIcon,
+			name: data.contact.link?.name,
+			type: 'other links'
+		}
 	]
 
 	return (
@@ -39,7 +60,7 @@ export function ButtonLink({ data }: { data: ResumeData }) {
 
 			<div className='hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex'>
 				{linkData
-					.filter((link) => link.url)
+					.filter((link) => ['website', 'email', 'phone'].includes(link.type))
 					.map((link, index) => (
 						<a key={index} href={link.url} target='_blank' rel='noreferrer'>
 							<span className='underline'>{link.url}</span>
